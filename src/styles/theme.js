409be9d43d1c845c36af9df9b2c64ef8fab104e8
Wellington Luiz Gaboardi <wgaboardi@gmail.com>
2022-09-02 20:52:00 -0300

    using GlobalStyles

---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    mainBg: 'blue',
+    secondaryBg: 'yellow',
+  },
+  fonts: {},
+  spacings: {},
+};
---
+++ src/templates/app/index.js
@@ -0,0 +1,13 @@
+import * as Styled from './styles';
+
+function Home() {
+  return (
+    <div className="App">
+      <Styled.Wrapper background="pink">
+        <h1>Alo</h1>
+      </Styled.Wrapper>
+    </div>
+  );
+}
+
+export default Home;
